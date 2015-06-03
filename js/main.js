0cd2a59d2af00851d2b469aeb6eb0684d42d4af7
Irene <wasp.ls.ia@gmail.com>
2015-06-03 10:13:17 +0200

    popups

--- js/main.js
+++ js/main.js
@@ -1052,43 +1052,50 @@ $('.show-login-curso').click(function(e) {
     $('.close').click(function(e) {
       $('.masc:visible').hide();
     })
-    $('.show-registro-curso').click(function(e) {
+    $('.show-gracias-curso').click(function(e) {
       e.preventDefault();
-      $('#popup').load('popups.html #registro-curso', function() {
-        $('#registro-curso').show();
-        $('#registro-curso button').click(function(e) {
+      $('#popup').load('popups.html #gracias-curso', function() {
+        $('#gracias-curso').show();
+        $('.close').click(function(e) {
+          $('.masc:visible').hide();
+        })
+        $('.show-registro-curso').click(function(e) {
           e.preventDefault();
-          var email = $('#registro-curso input[name="email"]');
-          var label = email.siblings('label');
-          var passwordname = $('#registro-curso input[name="password"]');
-          var labelpass = passwordname.siblings('label');
-          var password = $.trim(passwordname.val());
-          var name = $('#registro-curso input[name="personal"]');
-          var labelname = name.siblings('label');
-          var personalname = $.trim(name.val());
-          if(personalname== "") {
-            name.addClass('error');
-            labelname.show();
-          } else {
-            name.removeClass('error');
-            labelname.hide();
-          }
-          if(validateEmail(email.val())) {
-            email.removeClass('error');
-            label.hide();
-          } else {
-            email.addClass('error');
-            label.show();
-          }
-          if(password.length >= 4 && password.length <= 15) {
-            passwordname.removeClass('error');
-            labelpass.hide();
-          } else {
-            passwordname.addClass('error');
-            labelpass.show();
+          $('#popup').load('popups.html #registro-curso', function() {
+            $('#registro-curso').show();
+            $('#registro-curso button').click(function(e) {
+              e.preventDefault();
+              var email = $('#registro-curso input[name="email"]');
+              var label = email.siblings('label');
+              var passwordname = $('#registro-curso input[name="password"]');
+              var labelpass = passwordname.siblings('label');
+              var password = $.trim(passwordname.val());
+              var name = $('#registro-curso input[name="personal"]');
+              var labelname = name.siblings('label');
+              var personalname = $.trim(name.val());
+              if(personalname== "") {
+                name.addClass('error');
+                labelname.show();
+              } else {
+                name.removeClass('error');
+                labelname.hide();
+              }
+              if(validateEmail(email.val())) {
+                email.removeClass('error');
+                label.hide();
+              } else {
+                email.addClass('error');
+                label.show();
+              }
+              if(password.length >= 4 && password.length <= 15) {
+                passwordname.removeClass('error');
+                labelpass.hide();
+              } else {
+                passwordname.addClass('error');
+                labelpass.show();
 
-          }
-        });
+              }
+            });
 $('.close').click(function(e) {
   $('.masc:visible').hide();
 })
@@ -1101,6 +1108,8 @@ $('.show-gracias-curso').click(function(e) {
     })
 
   })
+})
+})
 })
 })
 })
@@ -1228,49 +1237,49 @@ $('.show-login-duda').click(function(e) {
     $('.close').click(function(e) {
       $('.masc:visible').hide();
     })
-  $('.show-gracias-duda').click(function(e) {
-  e.preventDefault();
-  $('#popup').load('popups.html #gracias-duda', function() {
-    $('#gracias-duda').show();
-    $('.close').click(function(e) {
-      $('.masc:visible').hide();
-    })
-    $('.show-registro-duda').click(function(e) {
+    $('.show-gracias-duda').click(function(e) {
       e.preventDefault();
-      $('#popup').load('popups.html #registro-duda', function() {
-        $('#registro-duda').show();
-        $('#registro-duda button').click(function(e) {
+      $('#popup').load('popups.html #gracias-duda', function() {
+        $('#gracias-duda').show();
+        $('.close').click(function(e) {
+          $('.masc:visible').hide();
+        })
+        $('.show-registro-duda').click(function(e) {
           e.preventDefault();
-          var email = $('#registro-duda input[name="email"]');
-          var label = email.siblings('label');
-          var passwordname = $('#registro-duda input[name="password"]');
-          var labelpass = passwordname.siblings('label');
-          var password = $.trim(passwordname.val());
-          var name = $('#registro-duda input[name="personal"]');
-          var labelname = name.siblings('label');
-          var personalname = $.trim(name.val());
-          if(personalname== "") {
-            name.addClass('error');
-            labelname.show();
-          } else {
-            name.removeClass('error');
-            labelname.hide();
-          }
-          if(validateEmail(email.val())) {
-            email.removeClass('error');
-            label.hide();
-          } else {
-            email.addClass('error');
-            label.show();
-          }
-          if(password.length >= 4 && password.length <= 15) {
-            passwordname.removeClass('error');
-            labelpass.hide();
-          } else {
-            passwordname.addClass('error');
-            labelpass.show();
-          }
-        });
+          $('#popup').load('popups.html #registro-duda', function() {
+            $('#registro-duda').show();
+            $('#registro-duda button').click(function(e) {
+              e.preventDefault();
+              var email = $('#registro-duda input[name="email"]');
+              var label = email.siblings('label');
+              var passwordname = $('#registro-duda input[name="password"]');
+              var labelpass = passwordname.siblings('label');
+              var password = $.trim(passwordname.val());
+              var name = $('#registro-duda input[name="personal"]');
+              var labelname = name.siblings('label');
+              var personalname = $.trim(name.val());
+              if(personalname== "") {
+                name.addClass('error');
+                labelname.show();
+              } else {
+                name.removeClass('error');
+                labelname.hide();
+              }
+              if(validateEmail(email.val())) {
+                email.removeClass('error');
+                label.hide();
+              } else {
+                email.addClass('error');
+                label.show();
+              }
+              if(password.length >= 4 && password.length <= 15) {
+                passwordname.removeClass('error');
+                labelpass.hide();
+              } else {
+                passwordname.addClass('error');
+                labelpass.show();
+              }
+            });
 $('.close').click(function(e) {
   $('.masc:visible').hide();
 })
@@ -1363,7 +1372,14 @@ $('.contactar').click(function(e){
 $('.close').click(function(e) {
   $('.masc:visible').hide();
 })
-$('.show-login-contactar').click(function(e) {
+$('.show-gracias-contactar').click(function(e) {
+  e.preventDefault();
+  $('#popup').load('popups.html #gracias-contactar', function() {
+    $('#gracias-contactar').show();
+    $('.close').click(function(e) {
+      $('.masc:visible').hide();
+    })
+    $('.show-login-contactar').click(function(e) {
       e.preventDefault();
       $('#popup').load('popups.html #login-contactar', function() {
         $('#login-contactar').show();
@@ -1392,42 +1408,49 @@ $('.show-login-contactar').click(function(e) {
         $('.close').click(function(e) {
           $('.masc:visible').hide();
         })
-        $('.show-registro-contactar').click(function(e) {
+        $('.show-gracias-contactar').click(function(e) {
           e.preventDefault();
-          $('#popup').load('popups.html #registro-contactar', function() {
-            $('#registro-contactar').show();
-            $('#registro-contactar button').click(function(e) {
+          $('#popup').load('popups.html #gracias-contactar', function() {
+            $('#gracias-contactar').show();
+            $('.close').click(function(e) {
+              $('.masc:visible').hide();
+            })
+            $('.show-registro-contactar').click(function(e) {
               e.preventDefault();
-              var email = $('#registro-contactar input[name="email"]');
-              var label = email.siblings('label');
-              var passwordname = $('#registro-contactar input[name="password"]');
-              var labelpass = passwordname.siblings('label');
-              var password = $.trim(passwordname.val());
-              var name = $('#registro-contactar input[name="personal"]');
-              var labelname = name.siblings('label');
-              var personalname = $.trim(name.val());
-              if(personalname== "") {
-                name.addClass('error');
-                labelname.show();
-              } else {
-                name.removeClass('error');
-                labelname.hide();
-              }
-              if(validateEmail(email.val())) {
-                email.removeClass('error');
-                label.hide();
-              } else {
-                email.addClass('error');
-                label.show();
-              }
-              if(password.length >= 4 && password.length <= 15) {
-                passwordname.removeClass('error');
-                labelpass.hide();
-              } else {
-                passwordname.addClass('error');
-                labelpass.show();
-              }
-            });
+              $('#popup').load('popups.html #registro-contactar', function() {
+                $('#registro-contactar').show();
+                $('#registro-contactar button').click(function(e) {
+                  e.preventDefault();
+                  var email = $('#registro-contactar input[name="email"]');
+                  var label = email.siblings('label');
+                  var passwordname = $('#registro-contactar input[name="password"]');
+                  var labelpass = passwordname.siblings('label');
+                  var password = $.trim(passwordname.val());
+                  var name = $('#registro-contactar input[name="personal"]');
+                  var labelname = name.siblings('label');
+                  var personalname = $.trim(name.val());
+                  if(personalname== "") {
+                    name.addClass('error');
+                    labelname.show();
+                  } else {
+                    name.removeClass('error');
+                    labelname.hide();
+                  }
+                  if(validateEmail(email.val())) {
+                    email.removeClass('error');
+                    label.hide();
+                  } else {
+                    email.addClass('error');
+                    label.show();
+                  }
+                  if(password.length >= 4 && password.length <= 15) {
+                    passwordname.removeClass('error');
+                    labelpass.hide();
+                  } else {
+                    passwordname.addClass('error');
+                    labelpass.show();
+                  }
+                });
 $('.close').click(function(e) {
   $('.masc:visible').hide();
 })
@@ -1439,6 +1462,10 @@ $('.show-gracias-contactar').click(function(e) {
       $('.masc:visible').hide();
     })
   })
+})
+})
+})
+})
 })
 })
 })
